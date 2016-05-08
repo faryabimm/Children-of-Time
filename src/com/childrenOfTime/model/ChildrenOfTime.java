e090cd64483cc0954d3855bdfa05d79d48ed8a13
faryabimohammadmahdi@gmail.com <faryabimohammadmahdi@gmail.com>
2016-05-08 15:49:32 +0430

    commit #5 statting implementation of Player class

--- src/com/childrenOfTime/model/ChildrenOfTime.java
+++ src/com/childrenOfTime/model/ChildrenOfTime.java
@@ -1,11 +1,16 @@
 package com.childrenOfTime.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by mohammadmahdi on 5/7/16.
  */
 public final class ChildrenOfTime {
 
     private static ChildrenOfTime instance;
+    private ArrayList<Player> players = new ArrayList<>();
+    private ArrayList<Battle> battles = new ArrayList<>();
+
 
     public static ChildrenOfTime getInstance() {
         if (instance == null) {
--- src/com/childrenOfTime/model/Player.java
+++ src/com/childrenOfTime/model/Player.java
@@ -1,7 +1,32 @@
 package com.childrenOfTime.model;
 
+import com.childrenOfTime.exceptions.NoImmortalityPotionLeftException;
+import com.childrenOfTime.exceptions.TradeException;
+import com.childrenOfTime.exceptions.UpgradeException;
+
+import java.util.ArrayList;
+
 /**
  * Created by mohammadmahdi on 5/8/16.
  */
 public class Player {
+
+    private int currentWealth;
+    private int currentExperience;
+    private int immprtalityPotions = 3;
+    private ArrayList<Hero> heros = new ArrayList<>();
+
+
+    public void upgradeAbility(Ability ability, Hero target) throws UpgradeException {
+    }
+
+    public void buy(Item item, Hero target) throws TradeException {
+    }
+
+    public void sell(Item item, Hero target) throws TradeException {
+    }
+
+    public void useImmortalityPotion(Hero target) throws NoImmortalityPotionLeftException {
+    }
+
 }
