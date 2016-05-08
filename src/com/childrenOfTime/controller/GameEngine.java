117ec23a417bdbb30099d43d8c1038d96811eef6
faryabimohammadmahdi@gmail.com <faryabimohammadmahdi@gmail.com>
2016-05-08 15:37:54 +0430

    commit #4 singelton implementation of ChildrenOfTime class

--- src/com/childrenOfTime/controller/GameEngine.java
+++ src/com/childrenOfTime/controller/GameEngine.java
@@ -1,7 +1,20 @@
 package com.childrenOfTime.controller;
 
+import com.childrenOfTime.model.ChildrenOfTime;
+
 /**
  * Created by mohammadmahdi on 5/7/16.
  */
 public class GameEngine {
+
+
+    private ChildrenOfTime childrenOfTime = ChildrenOfTime.getInstance();
+
+    public void startGame() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
