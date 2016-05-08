a84bcafae9a35e030f7eca03812a2e0af855947f
faryabimohammadmahdi@gmail.com <faryabimohammadmahdi@gmail.com>
2016-05-08 23:12:08 +0430

    commit #8 revision

--- src/com/childrenOfTime/model/Battle.java
+++ src/com/childrenOfTime/model/Battle.java
@@ -1,7 +1,39 @@
 package com.childrenOfTime.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by mohammadmahdi on 5/8/16.
  */
 public class Battle {
+
+    private ArrayList<Foe> foes = new ArrayList<>();
+
+    public void playStory() {
+    }
+
+    public void upgradeSession() {
+    }
+
+    public void storeSession() {
+    }
+
+    public void defeat() {
+    }
+
+    public void victory() {
+    }
+
+    public void giveRewards() {
+    }
+
+    public void showCurrentStats() {
+    }
+
+    public void help() {
+    }
+
+    public void fightSession() {
+    }
+
 }
