41be638bd027aabb6ee4277ddec5c3c7fb06206a
faryabimohammadmahdi@gmail.com <faryabimohammadmahdi@gmail.com>
2016-05-08 16:16:25 +0430

    commit #6 starting implementation of Warrior, Hero & Foe  added 2 enumerations for foe class and strength

--- src/com/childrenOfTime/model/Hero.java
+++ src/com/childrenOfTime/model/Hero.java
@@ -1,8 +1,55 @@
 package com.childrenOfTime.model;
 
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+
 /**
  * Created by mohammadmahdi on 5/8/16.
  */
-public class Hero extends Warrior {
+public abstract class Hero extends Warrior {
+
+    protected int inventorySize;
+    protected int maxMagic;
+    protected int magicRefillRate;
+    protected int currentMagic;
+    protected int currentEnergyPoints;
+    protected Inventory inventory;
+    protected ArrayList<Ability> abilities = new ArrayList<>();
+
+    public abstract void castAbility1();
+
+    public abstract void castAbility2();
+
+    public abstract void castAbility3();
+
+    public abstract void castAbility4();
+
+    public void attack() {
+    }
+
+    public ArrayList<Item> getInventoryItems() {
+
+        //TODO implement
+        return null;
+    }
+
+    public void addToInventory(Item item) {
+    }
+
+    public void removeFromInventory(Item item) {
+    }
+
+    public void applyItem(Item item) {
+    }
+
+    public abstract void die();
+
+
+
+
+
+
+
+
 
 }
