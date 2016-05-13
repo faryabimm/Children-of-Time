60c7b7359c2ab7a6b0a7524c5574feb31778522a
faryabimohammadmahdi@gmail.com <faryabimohammadmahdi@gmail.com>
2016-05-13 11:01:52 +0430

    commit refinements as always!!

--- src/com/childrenOfTime/exceptions/ItemNotAquiredException.java
+++ src/com/childrenOfTime/exceptions/ItemNotAquiredException.java
@@ -4,4 +4,7 @@ package com.childrenOfTime.exceptions;
  * Created by mohammadmahdi on 5/8/16.
  */
 public class ItemNotAquiredException extends AttackException {
+    public ItemNotAquiredException(String message) {
+        super();
+    }
 }
